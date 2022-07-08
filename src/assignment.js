38d02c147d9ac016f04810ca69711fdddd7246b5
Promise Osho <104498259+Promise152@users.noreply.github.com>
2022-07-08 07:54:14 +0000

    Completed Challenge 1 & 2 declaring a function using the for loop

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,15 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
+let sum = 0
+function sumOfNumbers(arrayOfNumbers) {for(i=0;i<arrayOfNumbers.length;i++)
+  {
+    sum+= arrayOfNumbers[i]
+  }
+  return sum
     return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +32,18 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+let count = 0
+function countEvenNumbers(arrayOfNumbers){for(let i of arrayOfNumbers){
+  if(i%2==0){
+    count+= 1; 
+  } 
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+        return count                                    
+    // return arrayOfNumbers.length;
+                                          }
+                                        
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
